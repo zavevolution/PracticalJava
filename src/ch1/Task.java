d0a06b0f83112416204612f6de5b3467eef41b3c
Zahrafov <evolutionzav@gmail.com>
2024-03-02 19:52:19 +0100

    work

--- src/ch1/Task.java
+++ src/ch1/Task.java
@@ -1,7 +1,21 @@
 package ch1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task {
     public static void main(String[] args) {
         System.out.println("hello");
+        Map<Character, Integer> count = countDuplicateCharacters("Hello, toto");
+    }
+
+    public static Map<Character, Integer> countDuplicateCharacters(String str) {
+        Map<Character, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
+        return result;
     }
 }
