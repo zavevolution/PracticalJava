303fc46ab6f10e7b1dd33f220a37547d6bda6002
Zahrafov <evolutionzav@gmail.com>
2024-03-05 17:11:02 +0100

    work

--- src/book/Less.java
+++ src/book/Less.java
@@ -2,6 +2,14 @@ package book;
 
 public class Less {
     public static void main(String[] args) {
+        Box mybox = new Box();
+        double vol;
+        mybox.width = 10;
+        mybox.height = 20;
+        mybox.depth = 15;
 
+        vol = mybox.width * mybox.height * mybox.depth;
+
+        System.out.println("Обєм рівен " + vol);
     }
 }
