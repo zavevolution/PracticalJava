067bfd9fe92ee398d141ba11b140ae769cbf9b51
Zahrafov <evolutionzav@gmail.com>
2024-03-05 17:26:12 +0100

    work

--- src/book/Box.java
+++ src/book/Box.java
@@ -4,4 +4,9 @@ public class Box {
     double width;
     double height;
     double depth;
+
+    void volume() {
+        System.out.println("Обєм рівний ");
+        System.out.println(width * height * depth);
+    }
 }
